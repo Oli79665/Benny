f54a48dd938b44da805fc63820f73b4cde69b12a
Oli79665 <oliver.west1122@gmail.com>
2024-06-06 00:04:28 +0100

    feat: create ping command

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
-import {Client, Events} from "discord.js"
+import {Client, Events, GatewayIntentBits} from "discord.js"
 
-const client = new Client({intents:[]});
+const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
 client.once(Events.ClientReady, (c) => {
     console.log("Ready! Logged in as", c.user.tag)
 });
+
+client.on(Events.MessageCreate, (msg) => {
+    if (!msg.content.startsWith(Bun.env.PREFIX as string) || msg.author.bot) return;
+    if (msg.content.startsWith(Bun.env.PREFIX as string + "ping")) {
+
+        msg.channel.send("pong!")
+    }
+});
+
+
 
 client.login(Bun.env.DISCORD_TOKEN);
