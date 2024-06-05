aed512e3fd28ad06da74b76ebac6cebd5b92ab5a
Oli79665 <oliver.west1122@gmail.com>
2024-06-06 00:46:28 +0100

    feat: added switch case

--- index.ts
+++ index.ts
@@ -1,17 +1,35 @@
-import {Client, Events, GatewayIntentBits} from "discord.js"
+import { Client, Events, GatewayIntentBits } from "discord.js"
 
-const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once(Events.ClientReady, (c) => {
     console.log("Ready! Logged in as", c.user.tag)
 });
 
-client.on(Events.MessageCreate, (msg) => {
-    if (!msg.content.startsWith(Bun.env.PREFIX as string) || msg.author.bot) return;
-    if (msg.content.startsWith(Bun.env.PREFIX as string + "ping")) {
+const prefix = Bun.env.PREFIX as string;
 
-        msg.channel.send("pong!")
+client.on(Events.MessageCreate, (msg) => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    switch (msg.content.slice(prefix.length)) {
+        case "ping": {
+            msg.channel.send("Pong!")
+            break;
+
+        }
+
+        case "femboys": {
+            msg.channel.send({ files: [{ attachment: "./sex.gif", name: "sex.gif", description: "FUCKEN YES MATE" }] });
+            break;
+            
+
+        }
+        case "help": {
+            msg.channel.send("Nah soz mate")
+            break;
+        }
     }
+   
 });
 
 
